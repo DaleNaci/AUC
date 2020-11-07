c827da6745406a493ea06254bbf010b2410bdf1d
Peter <peter.lande@colorado.edu>
2020-11-06 18:02:17 -0700

    add_ids added

--- backend/commands.py
+++ backend/commands.py
@@ -60,7 +60,7 @@ def adjust_elo(entry, isCrewWin, isImp):
     return entry
 
 #The base entry for a new player
-baseentry = ['0', '', 'Silver 1', '300', '0', '0', '0', '0', '0', '0']
+baseentry = ['0', '','', 'Silver 1', '300', '0', '0', '0', '0', '0', '0']
 
 #A function to add a game to the database
 #Input: [names of players] [names of imps] isCrewWin
@@ -171,6 +171,15 @@ def add_ids(players):
     gc = gspread.service_account(filename='client_secret.json')
     names = [player[0] for player in players]
     entries = database.get_rows_by_name(names, gc)
+    new_entries = []
     for entry in entries:
-        player = list(filter(lambda x: ))
+        info = list(filter(lambda x: entry[2] in x, players))
+        player = info[0]
+        new_entry = baseentry.copy();
+        new_entry[1] = player[1]
+        new_entry[2] = player[0]
+        new_entries.append(new_entry)
+    database.update_entries(new_entries, gc)
+    retrun True
+
     
